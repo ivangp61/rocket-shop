6aebecd581b6dda03165678c5161fc22bded508e
ivangp61 <ivangp61@gmail.com>
2020-06-04 20:24:13 -0400

    Initial commit

---
+++ test/first_spec.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe("A feature", function(){
+  describe("A escenario", function(){
+    it("does something expected", function(){
+      assert(true);
+    });
+  });
+});
